ffa51c5de4cfbffae8b54ee3d80dfc303ea0ea83
Ren0503 <illuminati0440@gmail.com>
2025-09-08 21:31:02 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/tinh-tinh/elasticsearch.git
+
+go 1.24.1
+
+require (
+	github.com/elastic/elastic-transport-go/v8 v8.7.0 // indirect
+	github.com/elastic/go-elasticsearch/v9 v9.1.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/tinh-tinh/tinhtinh/v2 v2.3.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+)
---
+++ module.go
@@ -0,0 +1,35 @@
+package elasticsearch
+
+import (
+	es "github.com/elastic/go-elasticsearch/v9"
+	"github.com/tinh-tinh/tinhtinh/v2/core"
+)
+
+const ELASTICSEARCH = "ELASTICSEARCH"
+
+func ForRoot(config es.Config) core.Modules {
+
+	return func(module core.Module) core.Module {
+		esModule := module.New(core.NewModuleOptions{})
+
+		client, err := es.NewClient(config)
+		if err != nil {
+			panic(err)
+		}
+
+		esModule.NewProvider(core.ProviderOptions{
+			Name:  ELASTICSEARCH,
+			Value: client,
+		})
+
+		return esModule
+	}
+}
+
+func InjectClient(module core.RefProvider) *es.Client {
+	client, ok := module.Ref(ELASTICSEARCH).(*es.Client)
+	if !ok {
+		return nil
+	}
+	return client
+}
